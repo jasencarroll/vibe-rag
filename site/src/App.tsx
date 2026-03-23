84d5736a01b45893ff7671a79587aebdc1c22e47
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2026-03-23 14:04:30 -0400

    Restore top scroll on site route changes

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,12 +1,28 @@
-import { Route, Routes } from 'react-router';
+import { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router';
 import { Header } from './components/Header';
 import { HomePage } from './pages/HomePage';
 import { MarkdownPage } from './pages/MarkdownPage';
 import { docs } from './site-docs';
 
+function ScrollRestoration() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const search = new URLSearchParams(location.search);
+    if (search.get('section')) {
+      return;
+    }
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [location.pathname, location.search]);
+
+  return null;
+}
+
 export function App() {
   return (
     <div className="min-h-screen bg-background text-foreground">
+      <ScrollRestoration />
       <Header />
       <main className="mx-auto max-w-6xl px-6 pb-24 pt-14">
         <Routes>
